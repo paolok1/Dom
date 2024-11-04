ccff784e27175c06689d614ac8b4045cc1ac0bc4
paolok1 <gpaolok1@gmail.com>
2024-11-04 20:00:28 +0100

    selfwork1 dom

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-// Manipolazione del DOM
+let p = document.querySelectorAll('p');
 
-// Dom: Document Object model
+console.dir(p);
+
+let h1 = document.querySelector('h1');
+
+
+
+let changeColor = document.querySelector('#changeColor');
+changeColor.addEventListener('click', ()=>{
+    p.forEach((paragrafo)=>{
+        let red = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        let green = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        let blu = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        paragrafo.style.color = `rgb(${red}, ${green}, ${blu})`
+        });
+} );
+
+let bold = document.querySelector('#bold');
+bold.addEventListener('click', ()=>{
+    p.forEach((paragrafo)=>{
+    paragrafo.style.fontWeight = 'bold';
+ })
+ 
+});
+
+
+let show_hide = document.querySelector('#show_hide');
+show_hide.addEventListener('click', ()=>{
+    p.forEach((paragrafo)=>{
+        paragrafo.style.display = 'none';
+})
+
+});
+
+
+
+
 
 
 
